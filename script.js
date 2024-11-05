9c13519b05911a6b57623897a457d4efa3758ddc
NICOLLAS PEREIRA REZENDE <nicollaspereirarezende@outlook.com.br>
2024-11-05 15:47:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function scrollToSection(id) {
     });
 }
 
-
+// Função para adicionar animações aos elementos ao rolar a página
 function handleScroll() {
     const elements = document.querySelectorAll('.animate');
     elements.forEach((el) => {
@@ -15,7 +15,7 @@ function handleScroll() {
     });
 }
 
-
+// Debounce para melhorar a performance do evento de scroll
 function debounce(func, wait = 20, immediate = true) {
     let timeout;
     return function () {
@@ -34,7 +34,7 @@ function debounce(func, wait = 20, immediate = true) {
 
 window.addEventListener('scroll', debounce(handleScroll));
 
-
+// Função para rolar os projetos
 function scrollProjects(direction) {
     const container = document.querySelector('.project-cards');
     const scrollAmount = direction === 'right' ? 200 : -450;
@@ -44,22 +44,26 @@ function scrollProjects(direction) {
     });
 }
 
-
+// Função para atualizar a exibição dos cartões de projetos
 function updateCards() {
     const cards = document.querySelectorAll('.project-cards .card');
     const cardsToShow = 2;
     let currentCardIndex = 0;
 
-    cards.forEach((card, index) => {
-        if (
-            index >= currentCardIndex &&
-            index < currentCardIndex + cardsToShow
-        ) {
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
-        }
-    });
+    function showCards() {
+        cards.forEach((card, index) => {
+            if (
+                index >= currentCardIndex &&
+                index < currentCardIndex + cardsToShow
+            ) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    }
+
+    showCards();
 
     const prevButton = document.getElementById('prevButton');
     const nextButton = document.getElementById('nextButton');
@@ -67,26 +71,26 @@ function updateCards() {
     prevButton.addEventListener('click', () => {
         if (currentCardIndex > 0) {
             currentCardIndex -= cardsToShow;
-            updateCards();
+            showCards();
         }
     });
 
     nextButton.addEventListener('click', () => {
         if (currentCardIndex + cardsToShow < cards.length) {
             currentCardIndex += cardsToShow;
-            updateCards();
+            showCards();
         }
     });
 }
 
 updateCards();
 
-
+// Função para alternar o menu de navegação
 function toggleMenu() {
     document.querySelector('.nav-links').classList.toggle('active');
 }
 
-
+// Função para alternar o tema entre claro e escuro
 function toggleTheme() {
     const root = document.documentElement;
     const themeIcon = document.getElementById('theme-icon');
